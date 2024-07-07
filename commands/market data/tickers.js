764e22c5f9167d190a1c647388dfbeaf6d361b4d
ibnaleem <ibnaleem@outlook.com>
2024-07-07 20:44:03 +0100

    add symbol arg to search for tickers by symbol

--- commands/market data/tickers.js
+++ commands/market data/tickers.js
@@ -5,45 +5,57 @@ const blockchainApiKey = process.env.BLOCKCHAIN_API_KEY;
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('tickers')
-    .setDescription('Get a list of ticker symbols'),
+    .setDescription('Get a list of ticker symbols')
+    .addStringOption(option => 
+      option.setName('symbol')
+      .setDescription('Blockchain symbol identifier, e.g., BTC-USD')
+      .setRequired(false)
+    ),
 
-    async execute(interaction) {
-      const headers = {
-        'Accept': 'application/json',
-        'X-API-Token': blockchainApiKey
+  async execute(interaction) {
+    const symbol = interaction.options.getString("symbol") ?? null;
+    const headers = {
+      'Accept': 'application/json',
+      'X-API-Token': blockchainApiKey
     }
 
     try {
+      let response;
       const fetch = await import('node-fetch').then(module => module.default);
-      const response = await fetch('https://api.blockchain.com/v3/exchange/tickers', {
-        method: 'GET',
-        headers: headers
-    });
-    
-    const data = await response.json();
-    const dataFields = data.map(d => ({
-      name: `${d.symbol} - ${d.price_24h}`,
-      value: `Volume: ${d.volume_24h}, Last Trade Price: ${d.last_trade_price}`,
-      inline: true
-  }));
 
-    const slicedDataFileds = dataFields.slice(0, 25);
+      if (symbol) {
+        response = await fetch(`https://api.blockchain.com/v3/exchange/tickers/${symbol}`, {
+          method: 'GET',
+          headers: headers
+        });
+      } else {
+        response = await fetch(`https://api.blockchain.com/v3/exchange/tickers`, {
+          method: 'GET',
+          headers: headers
+        });
+      }
 
-    const tickersEmbed = new EmbedBuilder()
-          .setColor(0x0099ff)
-          .setTitle(`Tickers List`)
-          .addFields(slicedDataFileds)
-          .setTimestamp()
-          .setFooter({ text: 'Data provided by Blockchain.com' });
-    
-    
-    await interaction.reply({ embeds: [tickersEmbed] });
+      const data = await response.json();
+      const dataFields = data.map(d => ({
+        name: `${d.symbol} - ${d.price_24h}`,
+        value: `Volume: ${d.volume_24h}, Last Trade Price: ${d.last_trade_price}`,
+        inline: true
+      }));
 
-    }
+      const slicedDataFields = dataFields.slice(0, 25);
 
-    catch (error) {
+      const tickersEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle('Tickers List')
+        .addFields(slicedDataFields)
+        .setTimestamp()
+        .setFooter({ text: 'Data provided by Blockchain.com' });
+
+      await interaction.reply({ embeds: [tickersEmbed] });
+
+    } catch (error) {
       console.error('Error fetching order book data:', error);
       await interaction.reply(`There was an error fetching the order book data for ${symbol}. Please try again later.`);
-
     }
-}}
+  }
+};
