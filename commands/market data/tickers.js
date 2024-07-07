415a316a41811312991f4faea5eafb902abbdc26
ibnaleem <ibnaleem@outlook.com>
2024-07-07 21:00:57 +0100

    fix data.map is not a function

--- commands/market data/tickers.js
+++ commands/market data/tickers.js
@@ -18,29 +18,44 @@ module.exports = {
       'Accept': 'application/json',
       'X-API-Token': blockchainApiKey
     }
+    
+    let data;
+    let dataFields;
+
 
     try {
-      let response;
       const fetch = await import('node-fetch').then(module => module.default);
 
       if (symbol) {
-        response = await fetch(`https://api.blockchain.com/v3/exchange/tickers/${symbol}`, {
+        url = `https://api.blockchain.com/v3/exchange/tickers/${symbol}`
+        const response = await fetch(url, {
           method: 'GET',
           headers: headers
         });
-      } else {
-        response = await fetch(`https://api.blockchain.com/v3/exchange/tickers`, {
-          method: 'GET',
-          headers: headers
-        });
-      }
 
-      const data = await response.json();
-      const dataFields = data.map(d => ({
+        data = await response.json();
+        const dataArray = [data];
+        dataFields = dataArray.map(d => ({
         name: `${d.symbol} - ${d.price_24h}`,
         value: `Volume: ${d.volume_24h}, Last Trade Price: ${d.last_trade_price}`,
         inline: true
       }));
+      } else {
+        url = 'https://api.blockchain.com/v3/exchange/tickers/'
+
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: headers
+        });
+
+        data = await response.json();
+        dataFields = data.map(d => ({
+        name: `${d.symbol} - ${d.price_24h}`,
+        value: `Volume: ${d.volume_24h}, Last Trade Price: ${d.last_trade_price}`,
+        inline: true
+        }));
+      }
+
 
       const slicedDataFields = dataFields.slice(0, 25);
 
